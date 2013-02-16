8a46cb4ca99a4ccb4ded7f91fe07730a5671489b
Jarek Piórkowski <jarek@piorkowski.ca>
2013-02-16 00:30:24 -0800

    unit test for known working cities' data

--- test_climate.py
+++ test_climate.py
@@ -53,10 +53,57 @@ class known_values(unittest.TestCase):
 		self.assertEqual(data['high'][0], 18.6)
 		self.assertEqual(data['record low'][11], 4.3)
 
-	# TODO: test with some known data
-	# (Vancouver, Toronto, Calgary, Melbourne, etc)
-	# to make sure future changes don't mess up known-working 
-	# queries
-	
+	def test_known_data(self):
+		"""Test for correct retrieval of some data for some cities.
+		Test against known-correct values retrieved via browser at time
+		of test authoring.
+		Basically make sure future changes don't mess up known-working
+		queries."""
+
+		known_data = {
+			'Vancouver': {
+				'record high': {6: 31.7}, #july
+				'mean': {9: 11.1}, #october
+				'precipitation mm': {7: 50.8} #august
+			},
+			'Seattle': {
+				# temperatures here also test conversion into C
+				'high': {3: 14.5}, #april
+				'record low': {1: -17.2, 7: 6.7}, #feb, aug
+				'precipitation days': {10: 18.4}, #november
+				'sun': {7: 248} #august
+				# TODO: test for conversion in -> cm/mm
+			},
+			'Calgary': {
+				'snow cm': {2: 21.9}, #march
+				'sun': {6: 314.9}, #july
+				'mean': {4: 9.8}, #may
+				'low': {10: -8.9}, #november
+				'record low': {1: -45} #february
+			},
+			'Melbourne': {
+				'record high': {1: 46.4}, #february
+				'record low': {6: -2.8}, #july
+				'sun': {5: 108}, #june
+			},
+			'Toronto': {
+				'mean': {6: 22.2}, #july
+				'snow days': {0: 12.0, 4: 0}, #january, may
+				'record low': {11: -30}, #december
+				'rain mm': {7: 79.6}, #august
+				'sun': {4: 229.1} #may
+			}
+		}
+
+		for city,data in known_data.items():
+			actual_data = climate.get_climate_data(city)
+
+			for key,key_data in data.items():
+				for month,expected_value in key_data.items():
+					self.assertEqual(
+						actual_data[key][month],
+						expected_value)
+
+
 if __name__ == '__main__':
 	unittest.main()
